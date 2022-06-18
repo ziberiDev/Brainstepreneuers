2c70cbf7860f7a6b0cbcfba0067d7853fd7195cc
Denis Ziberi <denis24951@gmail.com>
2022-06-18 14:59:14 +0200

    use mix to load css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <title>Brainsterpreneuers</title>
 
     <link rel="stylesheet" href="{{mix('/css/app.css') }}">
-    <link rel="stylesheet" href="{{asset('/css/main.css') }}">
+    <link rel="stylesheet" href="{{mix('/css/main.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 
